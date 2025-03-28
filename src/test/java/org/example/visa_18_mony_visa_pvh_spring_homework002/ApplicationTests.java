bed9ca5d6c6b32a93f60554dc0c32008ff046a52
MonyVisaSU4 <visagoodboy042@gmail.com>
2025-03-28 13:50:21 +0700

    Homework003

---
+++ src/test/java/org/example/visa_18_mony_visa_pvh_spring_homework002/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.visa_18_mony_visa_pvh_spring_homework002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
